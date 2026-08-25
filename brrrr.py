8c99fef6812ab4b16e3bf02b97c577fba9df2ed7
munoz1252-commits <munoz1252@gmail.com>
2026-08-24 23:38:43 -0400

    Update brrrr.py

--- brrrr.py
+++ brrrr.py
@@ -105,7 +105,8 @@ base_price = estimated_arv if estimated_arv > 0 else 250000
 comp_streets = ["Sweetbrier Dr", "Saxon Blvd", "Lois Dr", "Deltona Blvd", "Howland Blvd", "Normandy Blvd"]
 seed_val = 42
 rng = np.random.default_rng(seed_val)
-generatseed_val = 42ed_comps = []
+seed_val = 42
+ed_comps = []
 for i in range(4):
     street_num = rng.integers(800, 1200)
     street_name = rng.choice(comp_streets)
