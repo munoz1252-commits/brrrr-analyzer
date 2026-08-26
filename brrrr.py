0653f0abd49b2858ad0552c598737e22eec9dcb3
munoz1252-commits <munoz1252@gmail.com>
2026-08-25 20:24:06 -0400

    Update brrrr.py

--- brrrr.py
+++ brrrr.py
@@ -3,7 +3,7 @@ from app_cma_module import (
     apply_workspace_styling,
     filter_comps_by_radius,
     render_export_section,
-)apply_workspace_styling()
+apply_workspace_styling()
 import streamlit as st
 import pandas as pd
 import numpy as np
